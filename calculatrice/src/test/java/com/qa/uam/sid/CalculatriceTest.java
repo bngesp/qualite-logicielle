0aa6dcd960f11e6847c0bbc3180eff8a37ac745d
bassirou ngom <bassiroungom@esp.sn>
2025-02-16 18:44:05 +0100

    add asserThrow

--- calculatrice/src/test/java/com/qa/uam/sid/CalculatriceTest.java
+++ calculatrice/src/test/java/com/qa/uam/sid/CalculatriceTest.java
@@ -1,14 +1,28 @@
 package com.qa.uam.sid;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CalculatriceTest {
+    Calculatrice calculatrice;  // attributs
+
+    @Before
+    public void instance(){
+        calculatrice = new Calculatrice();
+    }
+
+    @After
+    public void apres(){
+        calculatrice = null;
+    }
+
+
 
     @Test
     public void additionTest(){
-        Calculatrice calculatrice = new Calculatrice();
         int som = calculatrice.addition(4, 5);
         assertEquals(9, som);
     }
@@ -16,7 +30,6 @@ public class CalculatriceTest {
     @Test
     public void additionValeurNegativeParTry()
     {
-        Calculatrice calculatrice = new Calculatrice();
         try{
             calculatrice.addition(-1, 4);
             fail("l'addition ne marche pas");
@@ -27,7 +40,6 @@ public class CalculatriceTest {
 
     @Test
     public void additionValeurNegativeParAssertThrows(){
-        Calculatrice calculatrice = new Calculatrice();
 //        ArithmeticException exception = assertThrows(ArithmeticException.class, () -> calculatrice.addition(-1, 5));
 //        assertEquals("numerande negative", exception.getMessage());
 
@@ -36,11 +48,23 @@ public class CalculatriceTest {
 
     @Test(expected = ArithmeticException.class)
     public void additionValeurNegativeParAnnotation(){
-        Calculatrice calculatrice = new Calculatrice();
         calculatrice.addition(-1, 5);
     }
 
 
     // ecrire les methodes de tests de division et gerer la division par 0
+
+
+    /*
+        * Befor
+        * additionTest
+        * after
+     */
+
+    // Creer Deux classes extends Calculatrice
+    /*
+        * CalculatriceAvance : RacineCarre(Math.sqrt), Puissance(Math.pow)
+        * CalculatriceScientifique : logarithme(Math.log), sinus(Math.sin)
+     */
 
 }
